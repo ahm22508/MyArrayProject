49765cf917f608508f3c2bfb3b3885c293b073cc
ahm22508 <ahmadamr659@gmail.com>
2024-03-13 07:02:06 +0200

    a commit

--- src/AhmedAmr/Array.java
+++ src/AhmedAmr/Array.java
@@ -1,4 +1,44 @@
 package AhmedAmr;
 
+import java.util.Arrays;
+
 public class Array {
+    private int[] Arrayedora;
+    private int contadora;
+
+    public Array(int longitud) {
+        Arrayedora = new int[longitud];
+    }
+
+    public void print() {
+        for (int i = 0; i < contadora; i++)
+        System.out.println(Arrayedora[i]);
+    }
+
+    public void add(int NewValue) {
+        if (Arrayedora.length == contadora) {
+            int[] Nueva = new int[contadora * 2];
+            for (int i = 0; i < contadora; i++)
+                Nueva[i] = Arrayedora[i];
+            Arrayedora = Nueva;
+        }
+        Arrayedora[contadora++] = NewValue;
+    }
+
+    public void RemoveFrom(int indice) {
+        if (indice < 0 || indice >= contadora)
+            throw new IllegalArgumentException();
+        for (int i = indice; i < contadora - 1; i++)
+            Arrayedora[i] = Arrayedora[i + 1];
+
+        contadora--;
+    }
+
+    public void IndexOF(int index) {
+        for (int i = 0; i < contadora; i++)
+            if (Arrayedora[i] == index)
+                System.out.println(i);
+    }
+
 }
+
--- src/AhmedAmr/Java.java
+++ src/AhmedAmr/Java.java
@@ -1,32 +1,43 @@
 package AhmedAmr;
 
+import java.util.Arrays;
 import java.util.Scanner;
+
 
 public class Java {
     public static void main(String[] args) {
-        System.out.println("Hello🖐️... enter your four arithmetic operations, please...");
-        for (int x = 0; x < 4; x++) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("enter your first number...");
-            int val1 = scanner.nextInt();
-            System.out.println("enter your second number...");
-            int val2 = scanner.nextInt();
-            System.out.println("enter your arithmetic operator...");
-            String val3 = scanner.next();
+//        System.out.println("Hello🖐️... enter your four arithmetic operations, please...");
+//        for (int x = 0; x < 4; x++) {
+//            Scanner scanner = new Scanner(System.in);
+//            System.out.println("enter your first number...");
+//            int val1 = scanner.nextInt();
+//            System.out.println("enter your second number...");
+//            int val2 = scanner.nextInt();
+//            System.out.println("enter your arithmetic operator...");
+//            String val3 = scanner.next();
+//
+//            if (val3.equals("+")) {
+//                System.out.println("(" + "'" + val3 + "'" + "," + val1 + "," + val2 + ")" + "--> " + (val1 + val2));
+//            } else if (val3.equals("-")) {
+//                System.out.println("(" + "'" + val3 + "'" + "," + val1 + "," + val2 + ")" + "--> " + (val1 - val2));
+//            } else if (val3.equals("*")) {
+//                System.out.println("(" + "'" + val3 + "'" + "," + val1 + "," + val2 + ")" + "--> " + (val1 * val2));
+//            } else if (val3.equals("/")) {
+//                System.out.println("(" + "'" + val3 + "'" + "," + val1 + "," + val2 + ")" + "--> " + (val1 / val2));
+//            } else {
+//                System.out.println("incorrect entry. The program will shut down. you can Try again... Thanks 😊 ");
+//                break;
+//            }
+//        }
+Array arr = new Array(5);
+        arr.add(5);
+        arr.add(4);
+        arr.add(3);
+        arr.add(2);
+        arr.add(1);
+        arr.print();
 
-            if (val3.equals("+")) {
-                System.out.println("(" + "'" + val3 + "'" + "," + val1 + "," + val2 + ")" + "--> " + (val1 + val2));
-            } else if (val3.equals("-")) {
-                System.out.println("(" + "'" + val3 + "'" + "," + val1 + "," + val2 + ")" + "--> " + (val1 - val2));
-            } else if (val3.equals("*")) {
-                System.out.println("(" + "'" + val3 + "'" + "," + val1 + "," + val2 + ")" + "--> " + (val1 * val2));
-            } else if (val3.equals("/")) {
-                System.out.println("(" + "'" + val3 + "'" + "," + val1 + "," + val2 + ")" + "--> " + (val1 / val2));
-            } else {
-                System.out.println("incorrect entry. The program will shut down. you can Try again... Thanks 😊 ");
-                break;
-            }
-        }
+
 
 
     }
