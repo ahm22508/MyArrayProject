18f536d9bdb21ce0502d39f2c2cdc154f2658905
ahm22508 <ahmadamr659@gmail.com>
2024-03-22 01:45:08 +0200

    commit

--- src/AhmedAmr/Sequence.java
+++ src/AhmedAmr/Sequence.java
@@ -1,32 +1,54 @@
 package AhmedAmr;
 
+import org.ietf.jgss.Oid;
+
 import java.util.Arrays;
 
 public class Sequence {
 
-    //[1 ,3, 6, 13]
-    public void ResolveSequence(int FinalNumber){
+    public void ResolveSequence(int FinalNumber) {
         int count = 4;
-        int LoopCount= 4;
-        int num= FinalNumber;
+        int LoopCount = 4;
+        int num = FinalNumber;
         int OtherNum = FinalNumber;
         int[] result = new int[count];
         for (int i = 0; i < LoopCount; i++) {
-            if(num == FinalNumber){
-                result[count-1] = num;
+            if (num == FinalNumber) {
+                result[count - 1] = num;
                 num--;
-            }
-            else{
-                int FinalNum = OtherNum/2;
+            } else {
+                int FinalNum = OtherNum / 2;
                 OtherNum = FinalNum;
-                result[count-2] =FinalNum;
+                result[count - 2] = FinalNum;
                 count--;
             }
+
         }
         System.out.println(Arrays.toString(result));
+
+    }
+    public void EvenArray(int Length) {
+        int x = 1;
+        int count =0;
+        int[] FirstArray = new int[Length];
+        for (int i = 0; i < Length; i++) {
+            FirstArray[i] = x;
+            x++;
+            if (i > 2 && i % 2 == 0) {
+                FirstArray[i] = i;
+                if(count==0) {
+                    System.out.print("["+FirstArray[i]+",");
+                }
+                if(count==1) {
+                    System.out.print(FirstArray[i]+",");
+                }
+                if (count==2) {
+                    System.out.println(FirstArray[i] + "]");
+                }
+                count++;
+            }
+
+        }
+
     }
 }
-
-
-
-
